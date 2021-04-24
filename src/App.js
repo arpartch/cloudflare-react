efdc10bc83223a5e410ddd2aeec22b0e2a920945
Athena <athena.partch@gmail.com>
2021-04-24 07:56:24 -0700

    update display text

--- src/App.js
+++ src/App.js
@@ -6,15 +6,8 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <h1>Amazing it works</h1>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Amazing CloudFlare works</h1>
+        <p>Deploy my React Apps to CloudFlare</p>
       </header>
     </div>
   );
